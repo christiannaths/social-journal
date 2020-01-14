cb6ca2d5c5faf92f40cfeea1b19f525c50647fcd
Christian Naths <christiannaths@gmail.com>
2020-01-13 22:21:48 -0600

    api | adds default controller actions

--- packages/api/app/models/journal.rb
+++ packages/api/app/models/journal.rb
@@ -2,4 +2,6 @@ class Journal < ApplicationRecord
   has_many :user_journals
   has_many :users, through: :user_journals
   has_many :entries
+
+  validates_uniqueness_of :namespace
 end
--- packages/api/config/routes.rb
+++ packages/api/config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :entries
   get '/health', to: 'health_checks#get'
   post '/health', to: 'health_checks#post'
 
-  resources :users
-  resources :journals
-  resources :entries
+  scope :v1 do
+    resources :users do
+      resources :journals, only: %i[index new create]
+    end
+    resources :journals, only: %i[show edit update destroy]
+    resources :entries
+  end
 end
