1922c22476d59bf0a698a27ffb91870b112c9f79
Christian Naths <christiannaths@gmail.com>
2020-02-03 12:37:52 -0600

    some readme stuff

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 ## Tools
 
 - https://www.htmlcsscolor.com/hex/00806C?fbclid=IwAR31wTrIpiqByuTZSKWc5WeGkUo_b9OomHOtnaSG2FPNlvAmIjKhJJndYdM
+
+## Articles
+
+- https://levelup.gitconnected.com/code-splitting-for-libraries-bundling-for-npm-with-rollup-1-0-2522c7437697
--- packages/webapp/src/App/styleTypography.js
+++ packages/webapp/src/App/styleTypography.js
@@ -3,32 +3,32 @@ import { createGlobalStyle } from 'styled-components/macro';
 const StyleBase = createGlobalStyle`
 
   h1 {
-    font-size: 5.653rem;
+    font-size: 5.653rem; /* 90px */
     font-weight: bold;
   }
 
   h2 {
-    font-size: 3.998rem;
+    font-size: 3.998rem; /* 60px */
     font-weight: bold;
   }
 
   h3 {
-    font-size: 2.827rem;
+    font-size: 2.827rem; /* 45px */
     font-weight: bold;
   }
 
   h4 {
-    font-size: 1.999rem;
+    font-size: 1.999rem; /* 32px */
     font-weight: bold;
   }
 
   h5 {
-    font-size: 1.414rem;
+    font-size: 1.414rem; /* 22.5px */
     font-weight: bold;
   }
 
   h6 {
-    font-size: 1rem;
+    font-size: 1rem; /* 16px */
     font-weight: bold;
   }
 
